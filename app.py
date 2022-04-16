921375733103d6204747ee026a513623bcff47de
jack <sxzhangjia@gmail.com>
2022-04-16 23:43:36 +0800

    dev: the APIs of actor; the detail of movie's API

--- app.py
+++ app.py
@@ -40,6 +40,19 @@ def create_app(test_config=None):
         })
 
 
+    @app.route('/api/movies/<int:id>', methods=['GET'])
+    def get_a_movie_detail(id):
+        movie = db.session.query(Movies).get(id)
+        if movie is None:
+            abort(404)
+        
+        return jsonify({
+            'success': True,
+            'movie': movie.format(),
+            'actors': [actor.format() for actor in movie.actors],
+        })
+
+
     @app.route('/api/movies', methods=['POST'])
     def post_a_movie():
         body = request.get_json()
@@ -60,19 +73,22 @@ def create_app(test_config=None):
         except:
             abort(422)
 
+        movie = Movies(title=title, release_date=release_date)
+        movie_id = None
+        
         try:
-            db.session.add(Movies(
-                title=title,
-                release_date=release_date))
+            db.session.add(movie)
             db.session.commit()
         except:
             db.session.rollback()
             abort(500)
         finally:
+            movie_id = movie.id
             db.session.close()
 
         return jsonify({
-            'success': True
+            'success': True,
+            'movie_id': movie_id,
         })
 
 
@@ -119,11 +135,12 @@ def create_app(test_config=None):
             abort(422)
         
         title = body.get('title')
-        release_date_string = body.get('release_date')
-
-        if title != None and title != '':
+        if title != None:
+            if title == '':
+                abort(422)
             movie.title = title
-        
+
+        release_date_string = body.get('release_date')
         if release_date_string != None and release_date_string != '':
             try:
                 movie.release_date = ReleaseDate\
@@ -140,7 +157,7 @@ def create_app(test_config=None):
             db.session.close()
 
         return jsonify({
-            'success': True
+            'success': True,
         })
 
 
@@ -161,25 +178,126 @@ def create_app(test_config=None):
 
         return jsonify({
             'success': True,
-            'movie_id': id,
         })
 
 
     @app.route('/api/actors', methods=['GET'])
     def get_actors():
-        pass
+        actors = db.session.query(Actors).all()
+        return jsonify({
+            'success': True,
+            'count': len(actors),
+            'actors': [actor.format() for actor in actors],
+        })
+
+
+    @app.route('/api/actors/<int:id>', methods=['GET'])
+    def get_an_actor_detail(id):
+        actor = db.session.query(Actors).get(id)
+        if actor is None:
+            abort(404)
+        
+        return jsonify({
+            'success': True,
+            'actor': actor.format(),
+            'movies': [movie.format() for movie in actor.movies],
+        })
+
 
     @app.route('/api/actors', methods=['POST'])
     def post_an_actor():
-        pass
+        body = request.get_json()
+        if body is None:
+            abort(422)
+        
+        name = body.get('name')
+        age = body.get('age')
+        gender = body.get('gender')
 
-    @app.route('/api/actors', methods=['PATCH'])
-    def patch_an_actor():
-        pass
+        if name is None or name == '' or\
+            age is None or isinstance(age, int) == False or\
+            gender is None or isinstance(gender, bool) == False:
+            abort(422)
+
+        actor = Actors(name=name, age=age, gender=gender)
+        actor_id = None
+        
+        try:
+            db.session.add(actor)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            abort(500)
+        finally:
+            actor_id = actor.id
+            db.session.close()
+
+        return jsonify({
+            'success': True,
+            'actor_id': actor_id,
+        })
+
+
+    @app.route('/api/actors/<int:id>', methods=['PATCH'])
+    def patch_an_actor(id):
+        actor = db.session.query(Actors).get(id)
+        if actor is None:
+            abort(404)
+
+        body = request.get_json()
+        if body is None:
+            abort(422)
+
+        name = body.get('name')
+        if name != None:
+            if name == '':
+                abort(422)
+            actor.name = name
+        
+        age = body.get('age')
+        if age != None:
+            if isinstance(age, int) == False:
+                abort(422)
+            actor.age = age
+
+        gender = body.get('gender')
+        if gender != None:
+            if isinstance(gender, bool) == False:
+                abort(422)
+            actor.gender = gender
+
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+            abort(500)
+        finally:
+            db.session.close()
+
+        return jsonify({
+            'success': True,
+        })
+
 
     @app.route('/api/actors/<int:id>', methods=['DELETE'])
     def delete_an_actor(id):
-        pass
+        actor = db.session.query(Actors).get(id)
+        if actor is None:
+            abort(404)
+        
+        try:
+            db.session.delete(actor)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            abort(500)
+        finally:
+            db.session.close()
+        
+        return jsonify({
+            'sucess': True,
+        })
+
 
     # @app.route('/login')
     # def login():
