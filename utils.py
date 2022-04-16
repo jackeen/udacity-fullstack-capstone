f7606ab2a0180246533ea031a792c86978fb6a8c
jack <sxzhangjia@gmail.com>
2022-04-16 16:08:42 +0800

    dev: adding utils release date converting for movie api

---
+++ utils.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+
+class ReleaseDate():
+
+	@classmethod
+	def date_string_to_object(self, date: str) -> datetime.date:
+		return datetime\
+        		.strptime(date, '%Y-%m-%d')\
+        		.date()
+
+	@classmethod
+	def date_object_to_string(self, date: datetime.date) -> str:
+		return ''
